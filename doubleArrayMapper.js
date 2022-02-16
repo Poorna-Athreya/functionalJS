4bad87e2d8365515caa089f6076d92a5dd839de0
Poorna-Athreya <Poorna_athreya@mckinsey.com>
2022-02-16 23:45:24 +0530

    Fix: Fixed test case errors

--- doubleArrayMapper.js
+++ doubleArrayMapper.js
@@ -1,7 +1,7 @@
 module.exports = doubleAll = (array) => {
-  if(array.constructor === Array) return 'Invalid input, please enter an array!';
+  if(array.constructor !== Array) return 'Invalid input, please enter an array!';
   if(array.length > 20) return 'Array size should be under 20!';
-  if(array.some((num) => { num > 9 || num < 0})) return 'Invalid array values, please enter numbers from 0-9!';
+  if(array.every((num) => { return num > 9 || num < 0;})) return 'Invalid array values, please enter numbers from 0-9!';
   return array.map((num) => {
         return num * 2;
   });
